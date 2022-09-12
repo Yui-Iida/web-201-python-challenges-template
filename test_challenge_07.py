5160b27b1403c8d9682062c1bac412091f068c6a
Yui-Iida <yxuxi328@icloud.com>
2022-09-12 19:26:42 +0200

    fixed 07

--- test_challenge_07.py
+++ test_challenge_07.py
@@ -9,9 +9,27 @@ def calculate_bill(shopping_list):
     total = 0
     for cost in shopping_list.values():
         total = total + cost
-        if(total >= 50):
-            total = total * 0.95
+        # if(total >= 50):
+        #     total = total * 0.95
+        # if total > 70:
+        #     total = total/100*5
+        # elif total > 60:
+        #     total = total/100*6
+        # elif total > 50:
+        #     total = total/100*7
+    # for item in shopping_list:
+    #     total = total + shopping_list[item]
+    print(total)      
+    if total > 70:
+        total = round((total * 0.93), 3)
+    elif total > 60:
+        total = round((total * 0.94), 3)
+    elif total > 50:
+        total = round((total * 0.95), 3)
+    else: total = total 
+        
     return total
+    
         
 
 def test_challenge_07_happy_case(): 
@@ -20,6 +38,6 @@ def test_challenge_07_happy_case():
 
 
 def test_challenge_07_happy_case2(): 
-    shopping_list2 = {'apples':50, 'bananas':60, 'eggs':50}
+    shopping_list2 = {'apples':11.20, 'bananas':12.2, 'eggs':30.00}
     # (50 + 60 + 50) * 0.95 = 152
-    assert calculate_bill(shopping_list2) == 152
+    assert calculate_bill(shopping_list2) == round((53.4 * 0.95) , 3)
